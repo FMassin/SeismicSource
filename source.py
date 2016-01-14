a48d2aa992d1df7f5502999895ff059f3c421aac
Frédérick Massin <fmassin@mun.ca>
2016-01-14 17:58:09 -0330

    Implementing seismsmicsoucemodel in class...
        - function in source.py are all working
        - class still some definition

--- source.py
+++ source.py
@@ -59,11 +59,175 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 #import obspy.imaging.scripts.mopad
 
-#class ShearTensile:
-#    def __init__(self):
-        
-def sheartensile_radpat(mt, wave='P', TKO=np.arange(0,181,1)*np.pi/180, AZM=np.arange(0,361,1)*np.pi/180, poisson=0.25) : 
+class SeismicSourceModel(object):
+
+    # general attribut definition
+    author = 'Ceci est a moi, A MOUHA, A-MOU-HAAAAAA'
+
+    def __init__(self, mt, wave, observation, poisson=0.25):
+
+        self.mt = mt
+        self.wave = wave
+        self.observation = observation
+        self.poisson = poisson
+
+    def sheartensite(self):
+
+        rd = radpat_sheartensile_vavryeuk(self.mt, self.wave) 
+
+        return rd
+
+    def surfacemotions(self,style='None'):
+
+        surfacemotion(self.mt, self.wave, self.poisson, style) 
+
+    def isotropiceffectonSP(self):
+
+        isotropiceffectonSP(self.mt)
+
+
+
+
+
+def farfield_radpat(mt, wave, points):
     """
+    Returns the P farfield radiation pattern
+    based on Aki & Richards Eq 4.29
+
+    :param mt: Focal mechanism NM x 6 (Mxx, Myy, Mzz, Mxy, Mxz, Myz - the
+               six independent components of the moment tensor)
+
+    :type wave: String
+    :param wave: type of wave to compute
+
+    :param points: 3D vector array with shape [3,npts] (x,y,z) or [2,npts]
+                   (theta,phi) The normalized displacement of the moment
+                   tensor source is computed at these points.
+
+    :return: 3D vector array with shape [3,npts] that contains the
+             displacement vector for each grid point
+    """
+    # Get full mt from 6-elt mt
+    Mpq = fullmt(mt)
+
+    ## Make sure we got np.array 
+    if np.asarray(points) is not points:
+        points = np.asarray(points)
+   
+    ndim, npoints = points.shape
+    if ndim == 2:
+        # points are given as theta,phi
+        newpoints = np.empty((3, npoints))
+        newpoints[0] = np.sin(points[0]) * np.cos(points[1])
+        newpoints[1] = np.sin(points[0]) * np.sin(points[1])
+        newpoints[2] = np.cos(points[0])
+        points = newpoints
+        ndim, npoints = points.shape
+    elif ndim == 3:
+        # points are given as x,y,z, (same system as the moment tensor)
+        pass
+    else:
+        raise ValueError('points should have shape 2 x npoints or 3 x npoints')
+
+    # precompute directional cosine array
+    dists = np.sqrt(points[0] * points[0] + points[1] * points[1] +
+                    points[2] * points[2])
+
+    # In gamma, all points are taken to a unit distance, same angle:
+    gammas = points / dists
+
+    # initialize displacement array
+    disp = np.empty((ndim, npoints))
+
+    # loop through points
+    for ipoint in range(npoints):
+        
+        gamma = gammas[:, ipoint]
+
+        if wave in ('S', 'S wave', 'S-wave'):
+
+            Mp = np.dot(Mpq, gamma)
+
+            # loop through displacement component [n index]
+            for n in range(ndim):
+                psum = 0.0
+                for p in range(ndim):
+                    deltanp = int(n == p)
+                    psum += (gamma[n] * gamma[p] - deltanp) * Mp[p]
+                disp[n, ipoint] = psum
+
+        elif wave in ('P', 'P wave', 'P-wave'):
+
+            gammapq = np.outer(gamma, gamma)
+            gammatimesmt = gammapq * Mpq
+            
+            # loop through displacement component [n index]
+            for n in range(ndim):
+                disp[n, ipoint] = gamma[n] * np.sum(gammatimesmt.flatten())
+
+    return disp, points
+
+def fullmt(mt):
+    """takes 6 comp moment tensor and returns full 3x3 moment tensor"""
+    mt_full = np.array(([[mt[0], mt[3], mt[4]],
+                         [mt[3], mt[1], mt[5]],
+                         [mt[4], mt[5], mt[2]]]))
+    return mt_full
+
+
+ 
+def farfield_surf(mt, wave='P',style='None') :     
+        
+    AZIMUTH = np.arange(0,365,5)*np.pi/180
+    TAKEOFF = np.arange(0,185,5)*np.pi/180
+    [AZIMUTH,TAKEOFF] = np.meshgrid(AZIMUTH,TAKEOFF)
+
+    points = [ np.reshape(TAKEOFF, (1,np.prod(AZIMUTH.shape)))[0].tolist() ,  np.reshape(AZIMUTH, (1,np.prod(TAKEOFF.shape)))[0].tolist() ]
+    
+    # Get radiation pattern
+    G, pointscart = farfield_radpat(mt, wave, points)
+
+    # Take a cartesian radpat, make a spherical (much easier to plot)
+    magn = np.sum(G * pointscart, axis=0)
+    magn /= np.max(np.abs(magn))
+    G = np.sqrt( G[0]**2 + G[1]**2 + G[2]**2 ) * magn
+    G = np.reshape(G, AZIMUTH.shape )
+
+    # Styles
+    if style in ('sign', 'bb', 'beachball', 'DC', 'fp'):
+        G[G < 0] = -1
+        G[G >= 0] = 1
+        scale=1
+    elif style == 'None' : 
+        scale = np.abs(G)
+    
+    Y = scale*np.cos(AZIMUTH) * np.sin(TAKEOFF)
+    X = scale*np.sin(AZIMUTH) * np.sin(TAKEOFF)
+    Z = scale*-np.cos(TAKEOFF)
+
+    # initializing the colormap machinery
+    norm = matplotlib.colors.Normalize(vmin=np.min(G),vmax=np.max(G))
+    c_m = matplotlib.cm.Spectral
+    s_m = matplotlib.cm.ScalarMappable(cmap=c_m, norm=norm)
+    s_m.set_array([])
+    
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')  
+    ax.plot_surface(X,Y,Z,linewidth=0, rstride=2, cstride=2, facecolors=s_m.to_rgba(G))
+    plt.colorbar(s_m)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show() 
+
+
+
+
+
+def radpat_sheartensile_vavryeuk(mt, wave='P', TKO=np.arange(0,181,1)*np.pi/180, AZM=np.arange(0,361,1)*np.pi/180, poisson=0.25) : 
+    """
+    I don't understand how the rake is used in this function
+    It seems to be a little different than farfield_radpat 
+
     rpgen(strike,dip,rake,isotropic,poisson, TKO, AZM) calculates P-wave, S-wave,
     SH-wave and SV-wave radiation pattern unp.sing shear-tensile source model
     presented in [see references 2, 3, 4 for details]. All input angles 
@@ -74,20 +238,26 @@ def sheartensile_radpat(mt, wave='P', TKO=np.arange(0,181,1)*np.pi/180, AZM=np.a
 
     :type strike, dip, rake: ObsPy : variables
     :param strike, dip, rake: fault plane parameters (radians).
+    
     :type isotropic: variable
     :param isotropic: tensile angle in radians (0 radians for pure shear faulting, 
         pi/2 radians for pure tensile opening).
+    
     :type poisson: variable
     :param poisson:  Poisson's ratio.
+    
     :type TKO: variable
     :param TKO: matrix of takeoff angles for which to calculate the 
         corresponding radiation pattern coefficients (radians, the takeoff 
         angles are measured from bottom).
+    
     :type AZM: variable
     :param AZM: matrix of corresponding azimuths (in radians) for which the 
         radiation pattern coefficients should be calculated.
+    
     :type wave: String
     :param wave: type of wave to compute
+    
     :rtype Gp, Gs, Gsh, Gsv: vectors
     :return Gp, Gs, Gsh, Gsv: P-wave, S-wave, SH-wave, and SV-wave radiation 
          pattern coefficients calculated for corresponding takeoff angles 
@@ -218,14 +388,14 @@ def sheartensile_radpat(mt, wave='P', TKO=np.arange(0,181,1)*np.pi/180, AZM=np.a
     return G
 
 
-def sheartensile_surf(mt, wave='P', poisson=0.25, style='None') :     
+def surfacemotions(mt, wave='P', poisson=0.25, style='None') :     
         
     AZIMUTH = np.arange(0,365,5)*np.pi/180
     TAKEOFF = np.arange(0,185,5)*np.pi/180
     [AZIMUTH,TAKEOFF] = np.meshgrid(AZIMUTH,TAKEOFF)
 
 
-    G = sheartensile_radpat(mt, wave, TAKEOFF, AZIMUTH, poisson)
+    G = radpat_sheartensile_vavryeuk(mt, wave, TAKEOFF, AZIMUTH, poisson)
     
     #G[G < -10] = -np.inf
     #G[G > 10 ] = np.inf
@@ -251,16 +421,20 @@ def sheartensile_surf(mt, wave='P', poisson=0.25, style='None') :
     ax = fig.gca(projection='3d')  
     ax.plot_surface(X,Y,Z,linewidth=0, rstride=2, cstride=2, facecolors=s_m.to_rgba(G))
     plt.colorbar(s_m)
+    plt.xlabel('x')
+    plt.ylabel('y')
     plt.show() 
 
-def sheartensile_testisotropic():
+def isotropiceffectonSP(mt=[0, 0, 0, 0]):
 
     rms=[]
     pythagoresum=[]
     average=[]
+
     for x in range(0, 100):
-        Prd = sheartensile_radpat([0, 0, 0.5, x/100. ],'P') 
-        Srd = sheartensile_radpat([0, 0, 0.5, x/100. ],'S') 
+        mt[3] = x / 100.
+        Prd = radpat_sheartensile_vavryeuk(mt,'P') 
+        Srd = radpat_sheartensile_vavryeuk(mt,'S') 
         P = np.sum(Prd**2)/np.prod(Prd.shape) # classic rms
         S = np.sum(Srd**2)/np.prod(Srd.shape)
         rms.append(S/P)
